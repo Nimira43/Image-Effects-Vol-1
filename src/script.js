9d2946a37dfb21ca012dde206098c28a64e813d3
Nimira43 <n.rabone72@aol.com>
2024-08-25 07:09:32 +0100

    Adjusted file structure to match the starter provided by Bruno, deleted node modules and package lock and then re-installed.

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({
   color: 0xff4500,
-  wireframe: true
+  // wireframe: true
 })
 const mesh = new THREE.Mesh(geometry, material)
 
