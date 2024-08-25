50a2693e6cdace0ce378419a9373ffb2f0c1dd46
Nimira43 <n.rabone72@aol.com>
2024-08-25 06:46:16 +0100

    Rendered wireframe cube

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({
-  color: 0xff4500
+  color: 0xff4500,
+  wireframe: true
 })
 const mesh = new THREE.Mesh(geometry, material)
 
@@ -16,6 +17,7 @@ const sizes = {
 }
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 
+camera.position.z = 3
 scene.add(camera)
 
 const renderer = new THREE.WebGLRenderer({
