07ad73a5e8788ad114946986812144a95577963a
Nimira43 <n.rabone72@aol.com>
2024-08-27 17:47:23 +0100

    Updated

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ import * as THREE from 'three'
 import './style.css'
 
 const canvas = document.querySelector('canvas.webgl')
+
 const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({
